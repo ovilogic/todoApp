b527e95eb2ca9c5fd9fc914f76c3853b809c44d3
ovilogic <ovidiu.opreah@yahoo.com>
2022-03-20 17:26:02 +0000

    django backend all files included: ready to go

---
+++ todo/serializers.py
@@ -0,0 +1,9 @@
+# A serializer is a component that converts Django models to JSON objects and vice-versa.
+
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'completed')
