2359044ca00ae6446faa870e2413de99535a169d
ovilogic <ovidiu.opreah@yahoo.com>
2022-03-21 18:34:33 +0000

    just added a modal in components

---
+++ frontend/src/components/modal.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    MOdalFooter,
+    Form,
+    FormGroup,
+    Input,
+    Label,
+    ModalFooter
+} from 'reactstrap';
+
+const CustomModal = (props) => {
+    const [ activeItem, setActive] = useState(props.activeItem)
+
+    handleChange = e => {
+        let { name, value } = e.target;
+        if (e.target.type === 'checkbox') {
+            value = e.target.checked;
+        }
+        const activeItem = { ...activeItem, [name]: value };
+        setActive({activeItem})
+    }
+    return ( 
+        <Modal isOpen={true} toggle={toggle}>
+            <ModalHeader toggle={toggle}>To Do Item</ModalHeader>
+            <ModalBody>
+                <Form>
+                    <FormGroup>
+                        <Label for='title'>Title</Label>
+                        <Input
+                            type='text'
+                            name='title'
+                            value={activeItem.title}
+                            onChange={handleChange()}
+                            placeholder='enter a to do title'
+                            />
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label for='completed'>
+                            <Input
+                                type='checkbox'
+                                name='completed'
+                                checked={activeItem.completed}
+                                onChange={handleChange}
+                                />
+                                Completed
+                        </Label>
+                    </FormGroup>
+                </Form>
+            </ModalBody>
+            <ModalFooter>
+                <Button color='success' onClick={() => onSave({activeItem})}>
+                    Save
+                </Button>
+            </ModalFooter>
+        </Modal>
+     );
+}
+ 
+export default CustomModal;
