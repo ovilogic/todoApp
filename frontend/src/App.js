42ac78caabbf2fd4402f90af5d6fceef35e1bb23
ovilogic <ovidiu.opreah@yahoo.com>
2022-05-16 13:18:31 +0300

    Page now refreshes accordingly after hitting either mark or delete buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,57 +21,52 @@ function App() {
   const [show, setShow] = useState(false)
   const [res, setRes] = useState(null)
   const [active, setActive] = useState(1)
+  const [ load, setLoad ] = useState(false)
 
- 
 
   const link = 'http://localhost:8000/api/todos/'
 
-  const completed = function(item) {
-    item.completed = !item.completed;
-    fetch(link, {
-      method: 'POST',
-      headers: {"Content-type": "application/json; charset=UTF-8"},
-      body: JSON.stringify(item),})
-      .then(() => {
-        console.log('task marked as completed')
-    });
-  }
 
-
-
-
+ 
+  /* Delete item in database */
   const deleteItem = function(item) {
     fetch(link + item.id, {
       method: 'DELETE',
       headers: {"Content-type": "application/json; charset=UTF-8"}
     })
+    .then( () => {
+      res.results.splice(res.results.indexOf(item), 1)
+      setRes(res);
+    })
   };
       
-    
-
+  // The main chunk of the page. The list of tasks with buttons next:
   var renderItems = () => {
-    console.log(res)
+    
     if (res != null) {
       
     const newItems = res.results
       // .filter(item => item.completed == viewCompleted);
+      // This is where we get the item from.
     return newItems.map(item => (
       <li key={item.id}>
           <span
             title={item.description}>
               {item.title} - Completed: {String(item.completed)}   
               <button className='mark'
-               onClick={() => completed(item) }>
+               onClick={() => {
+                 completed(item);
+                 setLoad(!load)
+                  }
+                }>
                  {viewCompleted ? 'mark as incomplete' :
                   'mark as complete'}</button>
               <button className='delete'
-               onClick={() =>  {
-                deleteItem(item);
-                console.log('delete fetch follows')
-                res.results.splice(res.results.indexOf(item), 1)
-                setRes(res)
-                fetchPage(page);         
-                }}>
+                onClick={() =>  {
+                  deleteItem(item);
+                  setLoad(!load)
+                  }
+                }>
                  Delete
               </button>
 
@@ -88,17 +83,25 @@ function App() {
     setActive(1)
     }, [viewCompleted]);
 
+  useEffect(() => {
+    fetchPage(active);
+    renderPagination();
+    renderItems();
+    console.log('this is the Request object used: ', res)
+    console.log('useEffect cycle for load')
+  }, [load]);
+
  
-  
+  // Where we get the 'res' from. Basically, the response body: the data your 
+  // your API sends to the client.
   const fetchPage = function(number) {
     try {
       fetch('http://localhost:8000/api/todos/?page=' + number +
        '&completed=' + viewCompleted)
       .then( fetchedList => fetchedList.json() )
       .then( list => {
-       
         setRes(list);
-      
+        console.log('this is the list, aka the res object retrieved: ', list)
       })
     }  
     catch (eroare) {
@@ -106,7 +109,7 @@ function App() {
       }
   }
 
-  console.log('ran through main')
+  console.log('App rendered once')
 
   const fetchPageClick = function(e) {
     const pageIndex = e.target.text;
@@ -117,13 +120,14 @@ function App() {
   useEffect(() => {
     renderPagination()
     }, [active, res])
-  
+
+
+  /* This is where you get the 'active' page state from */
   const renderPagination = function() {
     let total = res != null ? res.count : 0;
     let pageCount = total / 5
     if (total % 5 > 0) pageCount++;
 
-    
     let items = [];
     
     for (let number = 1; number <= pageCount; number++) {
@@ -152,6 +156,25 @@ function App() {
   let completion = ''
   if (viewCompleted) completion = 'completed'
   else completion = 'not completed'
+
+  /* Modify item in database */
+  const completed = function(item) {
+    item.completed = !item.completed;
+  
+    fetch(link + item.id + '/', {
+      method: 'PUT',
+      headers: {"Content-type": "application/json; charset=UTF-8"},
+      body: JSON.stringify(item)})
+      .then( modifiedItem => modifiedItem.json() )
+      .then( () => {
+       
+        res.results.splice(res.results.indexOf(item), 1)
+        setRes(res);
+      });
+  }
+
+
+
 
   return (
    
@@ -184,7 +207,7 @@ function App() {
         <MyModal
         show={show}
         setShow={() => setShow(!show)}/>
-          {console.log(show, 'is show')}
+          
       </div>
             
           
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from .serializers import TodoSerializer
 from rest_framework import viewsets
 from .models import Todo
@@ -16,15 +15,39 @@ class TodoView(viewsets.ModelViewSet):
         """
         queryset = Todo.objects.all()
         completed = self.request.query_params.get('completed')
+
         if completed is not None:
             if completed == 'false':
                 completed = False
             else:
                 completed = True
             queryset = queryset.filter(completed=completed)
+
+
         return queryset
 
+    def put(self, request):
+        # So with the PUT method, the object id is included in the URL
+        # This gets us the object we want to update:
+        todo_object = Todo.objects.get()
+        print(todo_object)
 
+        data = request.data
+        if data.completed is not None:
+            if data.completed == 'false':
+                data = False
+            else:
+                data.completed = True
+        todo_object.completed = data.completed
+        # Does the function need to return anything? Or just modify the
+        # content in the background?
+        # The answer is no, it does not need to return anything.
+
+
+
+
+
+# This renders the basic, rudimentary home page:
 def something(request):
     return render(request, 'todo/home.html')
 
